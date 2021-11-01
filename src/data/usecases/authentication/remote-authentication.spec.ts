7cd3f3a216c48d74ff75e62d31a8bae442aca667
ebertmota <ebert.mota03@gmail.com>
2021-11-01 09:09:26 -0300

    refactor: use faker in RemoteAuthentication

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,19 +1,22 @@
+import faker from 'faker';
 import { HttpPostClient } from '../../protocols/http/http-post-client';
 import { HttpPostClientSpy } from '../../test/mock-http-client';
 import { RemoteAuthentication } from './remote-authentication';
 
 describe('RemoteAuthentication', () => {
   let httpPostClientSpy: HttpPostClient & any;
+  let url: string;
   let sut: RemoteAuthentication;
 
   beforeEach(() => {
+    url = faker.internet.url();
     httpPostClientSpy = new HttpPostClientSpy();
-    sut = new RemoteAuthentication('any_url', httpPostClientSpy);
+    sut = new RemoteAuthentication(url, httpPostClientSpy);
   });
 
   it('should call HttpClient with correct URL', async () => {
     await sut.auth();
 
-    expect(httpPostClientSpy.url).toBe('any_url');
+    expect(httpPostClientSpy.url).toBe(url);
   });
 });
