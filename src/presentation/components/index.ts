21c53652cd0db9e0e26545496e3ca008ad95920f
ebertmota <ebert.mota03@gmail.com>
2022-02-01 09:28:09 -0300

    refactor: add index files with multiple imports

---
+++ src/presentation/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Footer } from './footer/footer';
+export { default as LoginHeader } from './login-header/login-header';
+export * from './form-status/form-status';
+export * from './input/input';
+export * from './logo/logo';
+export * from './spinner/spinner';
