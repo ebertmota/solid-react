73c9ced69746804886f6359cd627f0e793facec2
ebertmota <ebert.mota03@gmail.com>
2022-02-21 09:44:41 -0300

    test: ensure Login starts inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -5,11 +5,19 @@ import { Login } from '..';
 describe('Login component', () => {
   it('should start with initial state', () => {
     const { getByTestId } = render(<Login />);
-    const errorWrap = getByTestId('error-wrap');
 
+    const errorWrap = getByTestId('error-wrap');
     expect(errorWrap.childElementCount).toBe(0);
 
     const submitButton = getByTestId('submit') as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
+
+    const emailStatus = getByTestId('email-status');
+    expect(emailStatus.title).toBe('Campo obrigatório');
+    expect(emailStatus.textContent).toBe('🔴');
+
+    const passwordStatus = getByTestId('password-status');
+    expect(passwordStatus.title).toBe('Campo obrigatório');
+    expect(passwordStatus.textContent).toBe('🔴');
   });
 });
