9834777af2d12a93441c0ecbe67c9ec99ff47b52
ebertmota <ebert.mota03@gmail.com>
2022-03-14 10:05:19 -0300

    feat: ensure ValidationComposite returns the first error if validation fails

--- src/application/validation/validators/composite/composite.spec.ts
+++ src/application/validation/validators/composite/composite.spec.ts
@@ -1,19 +1,39 @@
-import { mock } from 'jest-mock-extended';
+import { mock, MockProxy } from 'jest-mock-extended';
 import { FieldValidation } from '@/application/validation/protocols';
 import { ValidationComposite } from './composite';
 
 describe('ValidationComposite', () => {
-  it('should return error if any validation fails', () => {
-    const fieldValidation = mock<FieldValidation>();
-    fieldValidation.field = 'any_field';
-    const anotherFieldValidation = mock<FieldValidation>();
-    anotherFieldValidation.field = 'any_field';
-    anotherFieldValidation.validate.mockReturnValueOnce(new Error('any_error'));
+  let fieldValidation: MockProxy<FieldValidation>;
+  let anotherFieldValidation: MockProxy<FieldValidation>;
 
+  beforeAll(() => {
+    fieldValidation = mock();
+    fieldValidation.field = 'any_field';
+    anotherFieldValidation = mock<FieldValidation>();
+    anotherFieldValidation.field = 'any_field';
+  });
+
+  it('should return error if any validation fails', () => {
+    anotherFieldValidation.validate.mockReturnValueOnce(new Error('any_error'));
     const sut = new ValidationComposite([anotherFieldValidation]);
 
     const result = sut.validate('any_field', 'any_value');
 
     expect(result).toBe('any_error');
+  });
+
+  it('should return error the first error if validation fails', () => {
+    anotherFieldValidation.validate.mockReturnValueOnce(
+      new Error('first_error'),
+    );
+    fieldValidation.validate.mockReturnValueOnce(new Error('second_error'));
+    const sut = new ValidationComposite([
+      anotherFieldValidation,
+      fieldValidation,
+    ]);
+
+    const result = sut.validate('any_field', 'any_value');
+
+    expect(result).toBe('first_error');
   });
 });
