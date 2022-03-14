b28413fe13efee86f1605c001ab2983ce0c80652
ebertmota <ebert.mota03@gmail.com>
2022-03-14 09:45:15 -0300

    test: ensure MinLengthValidation returns error if value is valid

--- src/application/validation/validators/min-length/min-length.spec.ts
+++ src/application/validation/validators/min-length/min-length.spec.ts
@@ -2,11 +2,27 @@ import { InvalidFieldError } from '@/application/validation/errors';
 import { MinLengthValidation } from './min-length';
 
 describe('MinLengthValidation', () => {
-  it('should return error if values is invalid', () => {
+  it('should return error if values have less than provided characters', () => {
     const sut = new MinLengthValidation('field', 5);
 
     const error = sut.validate('123');
 
     expect(error).toEqual(new InvalidFieldError('field'));
+  });
+
+  it('should return falsy if values have provided characters', () => {
+    const sut = new MinLengthValidation('field', 5);
+
+    const error = sut.validate('12345');
+
+    expect(error).toBeFalsy();
+  });
+
+  it('should return falsy if values have more than provided characters', () => {
+    const sut = new MinLengthValidation('field', 5);
+
+    const error = sut.validate('12345678910');
+
+    expect(error).toBeFalsy();
   });
 });
