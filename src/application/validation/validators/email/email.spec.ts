badcd6e3cd774f811d53c3016e71c4853ce9c34b
ebertmota <ebert.mota03@gmail.com>
2022-03-14 09:47:53 -0300

    test: ensure EmailValidation returns an falsy if email is empty

--- src/application/validation/validators/email/email.spec.ts
+++ src/application/validation/validators/email/email.spec.ts
@@ -18,4 +18,12 @@ describe('EmailValidation', () => {
 
     expect(error).toBeFalsy();
   });
+
+  it('should return falsy if email is empty', () => {
+    const sut = new EmailValidation('email');
+
+    const error = sut.validate('');
+
+    expect(error).toBeFalsy();
+  });
 });
