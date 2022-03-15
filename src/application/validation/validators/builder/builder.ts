666beb59f411988e786a4e2e6c007a279dfbf9f4
ebertmota <ebert.mota03@gmail.com>
2022-03-15 09:37:40 -0300

    feat: ensure ValidationBuilder returns EmailValidation when email is used

--- src/application/validation/validators/builder/builder.ts
+++ src/application/validation/validators/builder/builder.ts
@@ -1,5 +1,6 @@
 import { RequiredFieldValidation } from '@/application/validation/validators/required-field/required-field';
 import { FieldValidation } from '@/application/validation/protocols';
+import { EmailValidation } from '..';
 
 export class ValidationBuilder {
   private constructor(
@@ -13,6 +14,11 @@ export class ValidationBuilder {
 
   required(): ValidationBuilder {
     this.validations.push(new RequiredFieldValidation(this.fieldName));
+    return this;
+  }
+
+  email(): ValidationBuilder {
+    this.validations.push(new EmailValidation(this.fieldName));
     return this;
   }
 
