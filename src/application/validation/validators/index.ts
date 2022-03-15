c88a1ddcf73ba2c96811178974f2887a5325f627
ebertmota <ebert.mota03@gmail.com>
2022-03-15 09:35:06 -0300

    feat: ensure ValidationBuilder return RequiredFieldValidation on required

---
+++ src/application/validation/validators/index.ts
@@ -0,0 +1,5 @@
+export * from './builder/builder';
+export * from './composite/composite';
+export * from './email/email';
+export * from './min-length/min-length';
+export * from './required-field/required-field';
