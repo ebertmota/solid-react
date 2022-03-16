5fda15a7221d87d209c884826478eb921c47180c
ebertmota <ebert.mota03@gmail.com>
2022-03-16 09:43:28 -0300

    feat: ignore main folder in test coverage

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
