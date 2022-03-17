5083df8a31635828bc67fccb186af3e33b37689b
ebertmota <ebert.mota03@gmail.com>
2022-03-17 09:22:40 -0300

    feat: add makeApiUrl factory

--- src/main/factories/data/usecases/remote-authentication.ts
+++ src/main/factories/data/usecases/remote-authentication.ts
@@ -1,7 +1,7 @@
 import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication';
 import { makeAxiosHttpClient } from '@/main/factories/infra/http/axios-http-client';
+import { makeApiUrl } from '@/main/factories/infra';
 
 export const makeRemoteAuthentication = (): RemoteAuthentication => {
-  const url = 'http://fordevs.herokuapp.com/api/login';
-  return new RemoteAuthentication(url, makeAxiosHttpClient());
+  return new RemoteAuthentication(makeApiUrl('/login'), makeAxiosHttpClient());
 };
