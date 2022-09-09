cde4df289de10842f83ec2c33ed58893468f0d44
ebertmota <ebert.mota03@gmail.com>
2022-09-09 12:01:18 -0300

    feat: add saveAccessToken in Login factory implementation

--- src/main/factories/application/pages/login.tsx
+++ src/main/factories/application/pages/login.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Login } from '@/application/pages';
 import { makeRemoteAuthentication } from '@/main/factories/data';
+import { makeSaveAccessToken } from '@/main/factories/data/usecases/save-access-token';
 import { makeLoginValidation } from './login-validation';
 
 export const makeLogin: React.FC = () => {
@@ -8,6 +9,7 @@ export const makeLogin: React.FC = () => {
     <Login
       authentication={makeRemoteAuthentication()}
       validation={makeLoginValidation()}
+      saveAccessToken={makeSaveAccessToken()}
     />
   );
 };
