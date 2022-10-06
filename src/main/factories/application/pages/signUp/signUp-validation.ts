c01e5e05e8b8c0d1b2c174b11f6bd7aaccaa43c5
ebertmota <ebert.mota03@gmail.com>
2022-10-06 10:14:17 -0300

    feat: add SignUpValidation factory

---
+++ src/main/factories/application/pages/signUp/signUp-validation.ts
@@ -0,0 +1,17 @@
+import {
+  ValidationBuilder,
+  ValidationComposite,
+} from '@/application/validation/validators';
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  return new ValidationComposite([
+    ...ValidationBuilder.field('name').required().min(5).build(),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().min(5).build(),
+    ...ValidationBuilder.field('passwordConfirmation')
+      .required()
+      .min(5)
+      .sameAs('password')
+      .build(),
+  ]);
+};
