08dad8547189e03888bdaef1dedac6a80e8e7ac4
ebertmota <ebert.mota03@gmail.com>
2022-10-06 10:14:31 -0300

    test: add SignUpValidation factory

---
+++ src/main/factories/application/pages/signUp/signUp-validation.spec.ts
@@ -0,0 +1,24 @@
+import {
+  ValidationBuilder,
+  ValidationComposite,
+} from '@/application/validation/validators';
+import { makeSignUpValidation } from './signUp-validation';
+
+describe('SignUpValidationFactory', () => {
+  it('should make ValidationComposite with correct validations', () => {
+    const sut = makeSignUpValidation();
+
+    expect(sut).toEqual(
+      new ValidationComposite([
+        ...ValidationBuilder.field('name').required().min(5).build(),
+        ...ValidationBuilder.field('email').required().email().build(),
+        ...ValidationBuilder.field('password').required().min(5).build(),
+        ...ValidationBuilder.field('passwordConfirmation')
+          .required()
+          .min(5)
+          .sameAs('password')
+          .build(),
+      ]),
+    );
+  });
+});
