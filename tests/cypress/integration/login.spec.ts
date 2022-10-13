ca821c0a81618d46559bf6f376d958dc5db376f4
ebertmota <ebert.mota03@gmail.com>
2022-10-13 09:44:31 -0300

    test: ensure Login present error message if invalid credentials are provided

--- tests/cypress/integration/login.spec.ts
+++ tests/cypress/integration/login.spec.ts
@@ -1,4 +1,6 @@
 describe('Login', () => {
+  const { baseUrl } = Cypress.config();
+
   beforeEach(() => {
     cy.visit('/login');
   });
@@ -47,5 +49,23 @@ describe('Login', () => {
 
     cy.getByTestId('submit').should('not.have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').focus().type('valid@email.com');
+    cy.getByTestId('password').focus().type('12345');
+
+    cy.getByTestId('submit').click();
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('default-error')
+      .should('not.exist')
+      .getByTestId('spinner')
+      .should('not.exist')
+      .getByTestId('default-error')
+      .should('contain.text', 'Credenciais inválidas');
+
+    cy.url().should('eq', `${baseUrl}/login`);
   });
 });
