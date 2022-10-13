ef636dfdd6b8b2e4056341fdcddfbc43d1419586
ebertmota <ebert.mota03@gmail.com>
2022-10-13 09:32:24 -0300

    test: ensure Login present error if form is invalid

--- tests/cypress/integration/login.spec.ts
+++ tests/cypress/integration/login.spec.ts
@@ -4,16 +4,33 @@ describe('Login', () => {
   });
 
   it('should load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readOnly');
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('contain.text', '🔴');
 
+    cy.getByTestId('password').should('have.attr', 'readOnly');
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('contain.text', '🔴');
 
     cy.getByTestId('submit').should('have.attr', 'disabled');
 
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('should present error if form is invalid', () => {
+    cy.getByTestId('email').focus().type('invalid_email');
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'The field email is invalid')
+      .should('contain.text', '🔴');
+
+    cy.getByTestId('password').focus().type('1234');
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'The field password is invalid')
+      .should('contain.text', '🔴');
+
+    cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
   });
 });
